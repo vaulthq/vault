874e2880aad5b059201cb75d16ff1a1f3e0adcea
Valdas Kalvaitis <valdas@kalvaitis.eu>
2014-05-07 14:05:33 +0300

    https on security change

--- app/routes.php
+++ app/routes.php
@@ -5,7 +5,7 @@ Route::get('/', function()
     $existing = Cookie::get('XSRF-TOKEN');
     if (is_null($existing)) {
         $value = md5(Session::token());
-        setcookie('XSRF-TOKEN', $value, time()+3600, '/', 'vault.datajob.lt', false, false);
+        setcookie('XSRF-TOKEN', $value, time()+3600, '/', 'vault.datajob.lt', true, false);
     }
 	return View::make('angular');
 });
