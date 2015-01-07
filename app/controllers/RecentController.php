63077e410c5d458fb1e48f8a450211b904daf4e6
Valdas Kalvaitis <valdas@kalvaitis.eu>
2015-01-07 13:48:45 +0200

    Recently accessed items display only unique entries.

--- app/controllers/RecentController.php
+++ app/controllers/RecentController.php
@@ -1,21 +1,21 @@
 <?php
 class RecentController extends \BaseController
 {
-	/**
-	 * Display a listing of the recently accessed resources.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		return DB::table('entry')
-            ->leftJoin('history', 'entry.id', '=', 'history.model_id')
-            ->leftJoin('project', 'entry.project_id', '=', 'project.id')
-            ->where('history.model', 'password')
-            ->where('history.user_id', Auth::user()->id)
-            ->select('entry.id', 'entry.name', 'entry.url', 'entry.project_id', 'history.created_at', 'project.name as project_name')
-            ->orderBy('history.id', 'desc')
-            ->take(15)
-            ->get();
-	}
+    /**
+     * Display a listing of the recently accessed resources.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return DB::select("SELECT entry.id, entry.name, entry.url, entry.project_id, entry.note, h.created_at, project.name AS project_name FROM history AS h
+LEFT JOIN history AS h2 ON h.model_id = h2.model_id AND h2.id > h.id AND h.model = 'password' AND h2.model = 'password'
+LEFT JOIN entry ON entry.id = h.model_id
+LEFT JOIN project ON project.id = entry.project_id
+WHERE h2.id IS NULL
+AND h.model = 'password'
+AND h.user_id = ?
+ORDER BY h.id DESC
+LIMIT 15", [Auth::user()->id]);
+    }
 }
