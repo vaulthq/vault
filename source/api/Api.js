db4f0e86b517d4b25e812b96e58fcc6de0503a37
Valdas Kalvaitis <valdas@kalvaitis.eu>
2015-01-13 11:44:46 +0200

    This is not used.

--- source/api/Api.js
+++ source/api/Api.js
@@ -9,7 +9,6 @@
             project: $resource("/api/project/:id", null, enableCustom),
             assignedTeams: $resource("/api/project/teams/:id", null, enableCustom),
             user: $resource("/api/user/:id", null, enableCustom),
-            showUser: $resource("/api/user/show", null),
             team: $resource("/api/team/:id", null, enableCustom),
             teamMembers: $resource("/api/teamMembers/:id", null, enableCustom),
             projectTeams: $resource("/api/projectTeams/:id", null, enableCustom),
