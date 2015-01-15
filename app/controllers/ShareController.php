2e99e26411842c3e8eef750fd7b0654e13166468
Valdas Kalvaitis <valdas@kalvaitis.eu>
2015-01-15 13:25:44 +0200

    Refactor entry share modal for upcoming feature - share to team.

--- app/controllers/ShareController.php
+++ app/controllers/ShareController.php
@@ -1,80 +1,88 @@
 <?php
 
-class ShareController extends \BaseController {
+class ShareController extends \BaseController
+{
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        $userId = Input::get('user_id');
+        $entryId = Input::get('id');
+
         $validator = Validator::make([
-            'user_id' => Input::get('user_id'),
-            'entry_id' => Input::get('id')
+            'user_id' => $userId,
+            'entry_id' => $entryId
         ], Share::$rules);
 
         if ($validator->fails()) {
             return Response::make($validator->messages()->first(), 419);
         }
 
-        $entry = Entry::findOrFail(Input::get('id'));
+        if (Share::where('user_id', $userId)->where('entry_id', $entryId)->count() > 0) {
+            return Response::make('This entry is already shared for this user.', 419);
+        }
+
+        $entry = Entry::findOrFail($entryId);
         if (!$entry->can_edit) {
             return Response::json(['flash' => 'Unauthorized.'], 403);
         }
 
         $model = new Share();
         $model->user_by_id = Auth::user()->id;
-        $model->user_id = Input::get('user_id');
-        $model->entry_id = Input::get('id');
+        $model->user_id = $userId;
+        $model->entry_id = $entryId;
         $model->save();
 
         History::make('share', 'Shared entry.', $model->id);
 
+        return Share::with('user')->where('id', $model->id)->first();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $model = Share::with('user')->where('entry_id', $id)->get();
+
         return $model;
-	}
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$model = Share::where('entry_id', $id)->get();
-
-        return $model;
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
         $model = Share::findOrFail($id);
 
         $entry = Entry::findOrFail($model->entry_id);
@@ -85,6 +93,6 @@ class ShareController extends \BaseController {
         History::make('entry', 'Deleted share.', $id);
 
         $model->delete();
-	}
+    }
 
 }
--- public/js/app.js
+++ public/js/app.js
@@ -655,48 +655,46 @@ xApp
         }
     });
 
-xApp
-    .controller('ModalShareController', function($scope, $modalInstance, users, access, Api, entry) {
+(function() {
+    angular
+        .module('xApp')
+        .controller('ModalShareController', shareController);
+
+    function shareController($scope, $modalInstance, users, access, Api, entry) {
         $scope.users = users;
         $scope.access = access;
         $scope.entry = entry;
 
-        $scope.canAccess = function(userId) {
-            return $scope.getAccessIndex(userId) != -1;
-        }
+        $scope.share = {
+            user: 0
+        };
 
-        $scope.getAccessIndex = function(userId) {
-            for (var i=0; i<$scope.access.length; i++) {
-                if ($scope.access[i].user_id == userId) {
-                    return i;
-                }
-            }
+        $scope.users.$promise.then(function() {
+            $scope.share.user = $scope.users[0].id;
+        });
 
-            return -1;
-        }
-
-        $scope.grant = function(userId) {
+        $scope.shareUser = function() {
             Api.share.save({
-                user_id: userId,
+                user_id: $scope.share.user,
                 id: $scope.entry.id
             }, function(response) {
                 $scope.access.push(response);
             });
         }
 
-        $scope.revoke = function(userId) {
-            var scopeIndex = $scope.getAccessIndex(userId);
+        $scope.revoke = function(accessId) {
             Api.share.delete({
-                id: $scope.access[scopeIndex].id
-            }, function(response) {
-                $scope.access.splice(scopeIndex, 1);
+                id: accessId
+            }, function() {
+                $scope.access.splice($scope.access.map(function(i) {return i.id;}).indexOf(accessId), 1);
             });
         }
 
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
         };
-    });
+    }
+})();
 
 xApp
     .controller('ModalUpdateEntryController', function($scope, $modalInstance, EntryFactory, shareFlash, entry, GROUPS) {
