807c9c594695d042b871260da10e733a8c6af31e
Vaidas Mikalauskas <vaidas.mikalauskas@gmail.com>
2015-01-15 13:32:02 +0200

    docker dev box

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'x_dev',
-			'username'  => 'x.dev',
-			'password'  => 'pass',
+			'database'  => 'vault',
+			'username'  => 'root',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- app/config/example/database.php
+++ app/config/example/database.php
@@ -8,9 +8,9 @@ return array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'x-share',
-            'username'  => 'homestead',
-            'password'  => 'secret',
+            'database'  => 'vault',
+            'username'  => 'root',
+            'password'  => '',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
