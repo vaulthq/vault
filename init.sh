17f5b7d83e0e3eb850fa068fbf6ee7f1d3ededf1
Valdas Kalvaitis <valdas@kalvaitis.eu>
2015-05-19 11:11:50 +0300

    Update init scripts

--- init.sh
+++ init.sh
@@ -4,18 +4,27 @@ echo "Creating vault database";
 docker-compose run --rm mysql bash -c 'mysql -u root -h mysql -psecret -e "CREATE DATABASE IF NOT EXISTS vault;"'
 
 echo "Granting write permissions to storage";
-chmod 777 -R app/storage
+chmod 777 -R api/storage
 
 echo "Copying default configs"
-if [ ! -d app/config/local ]; then
-    cp -R -u app/config/example app/config/local
+if [ ! -f api/.env ]; then
+    cp -u api/.env.sample api/.env
 fi
 
 echo "Running Composer";
-cdocker-compose run --rm php bash -c 'composer install -o'
+docker-compose run --rm php bash -c 'cd api; composer install -o'
 
 echo "Running migrations";
-docker-compose run --rm php bash -c 'php artisan migrate --no-interaction --env=local'
+docker-compose run --rm php bash -c 'php api/artisan migrate --no-interaction --env=local'
 
 echo "Running seeding";
-docker-compose run --rm php bash -c 'php artisan db:seed --no-interaction --env=local'
+docker-compose run --rm php bash -c 'php api/artisan db:seed --no-interaction --env=local'
+
+echo "Installing NPM";
+docker-compose run --rm webtools bash -c 'npm install'
+
+echo "Installing Bower";
+docker-compose run --rm webtools bash -c 'bower install'
+
+echo "Running Gulp";
+docker-compose run --rm webtools bash -c 'gulp'
