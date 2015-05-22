e3b66eb5007a024b16e20cfbad9d43b5b7fbbda9
Valdas Kalvaitis <valdas@kalvaitis.eu>
2015-05-22 14:08:37 +0300

    Synch

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 if [ -f /var/www/composer.json ] && [ ! -d /var/www/vendor ]; then
    echo "Composer in background"
-   composer install --optimize-autoloader --working-dir=/var/www/api &
+   composer install --optimize-autoloader --working-dir=/var/www/api
 fi
 
 chmod 777 -R /var/www/api/storage
