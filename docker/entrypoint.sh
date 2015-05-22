f925843116b3df6b2d561b6a40a6be12123a4363
Vaidas Mikalauskas <vaidas.mikalauskas@gmail.com>
2015-05-22 16:23:07 +0300

    no need for custom php build

---
+++ docker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+if [ ! -f /var/www/api/.env ]; then
+   echo "[info] copy .env"
+   cp /var/www/api/.env.sample /var/www/api/.env
+fi
+
+if [ -f /var/www/api/composer.json ] && [ ! -d /var/www/api/vendor ]; then
+   echo "[info] Composer in background"
+   composer install --optimize-autoloader --working-dir=/var/www/api
+fi
+
+chmod 777 -R /var/www/api/storage
+
+echo "[info] Migrating database"
+php /var/www/api/artisan migrate --no-interaction --env=local || true
+echo "[info] Seeding database"
+php /var/www/api/artisan db:seed --no-interaction --env=local || true
+
+echo "Run: $@"
+exec "$@"
