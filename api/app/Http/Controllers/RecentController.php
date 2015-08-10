1ffe68e35a931e5389bbff3f0ccd768cf7e03a6d
Valdas Kalvaitis <valdas@kalvaitis.eu>
2015-08-10 14:57:56 +0300

    Do not display keys for deleted projects

--- api/app/Http/Controllers/RecentController.php
+++ api/app/Http/Controllers/RecentController.php
@@ -15,7 +15,7 @@ class RecentController extends Controller
         return DB::select("SELECT entry.id, entry.name, entry.url, entry.project_id, entry.note, entry.username, h.created_at, project.name AS project_name FROM history AS h
 LEFT JOIN history AS h2 ON h.model_id = h2.model_id AND h2.id > h.id AND h.model = 'password' AND h2.model = 'password'
 LEFT JOIN entry ON entry.id = h.model_id AND entry.deleted_at IS NULL
-LEFT JOIN project ON project.id = entry.project_id
+LEFT JOIN project ON project.id = entry.project_id AND project.deleted_at IS NULL
 WHERE h2.id IS NULL
 AND h.model = 'password'
 AND h.user_id = ?
