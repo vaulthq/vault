df6efb840eed677bcfcd444edf67f6a929d6964b
Valdas Kalvaitis <valdas@kalvaitis.eu>
2015-08-24 14:39:28 +0300

    Make index page static, remove custom Docker image

--- api/public/js/app.js
+++ api/public/js/app.js
@@ -215,74 +215,6 @@
 (function() {
     angular
         .module('xApp')
-        .factory('Api', apiFactory);
-
-    function apiFactory($resource) {
-        return {
-            auth: $resource("/internal/auth"),
-            project: $resource("/api/project/:id", null, enableCustom),
-            projectKeys: $resource("/api/project/keys/:id"),
-            projectOwner: $resource("/api/project/changeOwner/:id", null, enableCustom),
-            assignedTeams: $resource("/api/project/teams/:id", null, enableCustom),
-            user: $resource("/api/user/:id", null, enableCustom),
-            apis: $resource("/api/apis/:id", null, enableCustom),
-            team: $resource("/api/team/:id", null, enableCustom),
-            teamMembers: $resource("/api/teamMembers/:id", null, enableCustom),
-            projectTeams: $resource("/api/projectTeams/:id", null, enableCustom),
-            entryTeams: $resource("/api/entryTeams/:id", null, enableCustom),
-            entryTags: $resource("/api/entryTags/:id", null, enableCustom),
-            authStatus: $resource("/internal/auth/status", null),
-            profile: $resource("/api/profile", null, enableCustom),
-            share: $resource("/api/share/:id", null, enableCustom),
-            entry: $resource("/api/entry/:id", null, angular.extend(enableCustom, {
-                password: { method: 'GET', params: {id: '@id'} }
-            })),
-            entryAccess: $resource("/api/entry/access/:id", null),
-            entryPassword: $resource("/api/entry/password/:id", {}, {
-                password: { method: 'GET', params: {id: '@id'} }
-            })
-        }
-    }
-
-    var enableCustom = {
-        update: {
-            method: 'PUT', params: {id: '@id'}
-        },
-        delete: {
-            method: 'DELETE', params: {id: '@id'}
-        }
-    };
-})();
-
-(function() {
-    angular
-        .module('xApp')
-        .controller('ApiController', ctrl);
-
-    function ctrl($scope, Api, apis) {
-        $scope.apis = apis;
-        $scope.revoke = revoke;
-
-        $scope.$on('key:create', onKeyCreate);
-
-        function revoke(api) {
-            if (!confirm('Are you sure?')) {
-                return;
-            }
-
-            Api.apis.remove({id: api.id});
-            $scope.apis.splice($scope.apis.map(function(x) { return x.id; }).indexOf(api.id), 1);
-        }
-
-        function onKeyCreate(e, key) {
-            $scope.apis.push(key);
-        }
-    }
-})();
-
-(function() {
-    angular
-        .module('xApp')
         .controller('AuthController', authController);
 
     function authController($scope, AuthFactory) {
@@ -428,6 +360,74 @@
     }
 
 })();
+(function() {
+    angular
+        .module('xApp')
+        .factory('Api', apiFactory);
+
+    function apiFactory($resource) {
+        return {
+            auth: $resource("/internal/auth"),
+            project: $resource("/api/project/:id", null, enableCustom),
+            projectKeys: $resource("/api/project/keys/:id"),
+            projectOwner: $resource("/api/project/changeOwner/:id", null, enableCustom),
+            assignedTeams: $resource("/api/project/teams/:id", null, enableCustom),
+            user: $resource("/api/user/:id", null, enableCustom),
+            apis: $resource("/api/apis/:id", null, enableCustom),
+            team: $resource("/api/team/:id", null, enableCustom),
+            teamMembers: $resource("/api/teamMembers/:id", null, enableCustom),
+            projectTeams: $resource("/api/projectTeams/:id", null, enableCustom),
+            entryTeams: $resource("/api/entryTeams/:id", null, enableCustom),
+            entryTags: $resource("/api/entryTags/:id", null, enableCustom),
+            authStatus: $resource("/internal/auth/status", null),
+            profile: $resource("/api/profile", null, enableCustom),
+            share: $resource("/api/share/:id", null, enableCustom),
+            entry: $resource("/api/entry/:id", null, angular.extend(enableCustom, {
+                password: { method: 'GET', params: {id: '@id'} }
+            })),
+            entryAccess: $resource("/api/entry/access/:id", null),
+            entryPassword: $resource("/api/entry/password/:id", {}, {
+                password: { method: 'GET', params: {id: '@id'} }
+            })
+        }
+    }
+
+    var enableCustom = {
+        update: {
+            method: 'PUT', params: {id: '@id'}
+        },
+        delete: {
+            method: 'DELETE', params: {id: '@id'}
+        }
+    };
+})();
+
+(function() {
+    angular
+        .module('xApp')
+        .controller('ApiController', ctrl);
+
+    function ctrl($scope, Api, apis) {
+        $scope.apis = apis;
+        $scope.revoke = revoke;
+
+        $scope.$on('key:create', onKeyCreate);
+
+        function revoke(api) {
+            if (!confirm('Are you sure?')) {
+                return;
+            }
+
+            Api.apis.remove({id: api.id});
+            $scope.apis.splice($scope.apis.map(function(x) { return x.id; }).indexOf(api.id), 1);
+        }
+
+        function onKeyCreate(e, key) {
+            $scope.apis.push(key);
+        }
+    }
+})();
+
 (function () {
     angular
         .module('xApp')
